96926103a0ed18093f8f4085572561da44074c60
Bruno Andreotti <bruno.andreotti6@gmail.com>
2019-05-30 13:49:12 -0300

    Interpretação de argumentos da linha de comando

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,46 @@ void print_vetor(int* vetor, int n) {
 
 int main(int argc, char* argv[]) {
     
-    int* vetor = GeradorVetor::aleatorio(100, 200);
-    print_vetor(vetor, 20);
-    auto resultado = Quicksort::nao_recursivo(vetor, 100);
+    if (argc < 4) {
+        std::cerr << "Número de argumentos incorreto" << std::endl;
+        return 1;
+    }
+
+    int n = std::stoi(argv[3]); 
+    int *vetor = nullptr;
+    
+    std::string tipo_vetor(argv[2]);
+    
+    if (tipo_vetor == "Ale")
+        vetor = GeradorVetor::aleatorio(n, 2 * n);
+    else if (tipo_vetor == "OrdC")
+        vetor = GeradorVetor::crescente(n);
+    else if (tipo_vetor == "OrdD")
+        vetor = GeradorVetor::decrescente(n);
+
     print_vetor(vetor, 20);
 
+    Resultado resultado;
+    
+    std::string tipo_algoritmo(argv[1]);
+
+    if (tipo_algoritmo == "QC")
+        resultado = Quicksort::classico(vetor, n, EscolhaPivo::central);
+    else if (tipo_algoritmo == "QM3")
+        resultado = Quicksort::classico(vetor, n, EscolhaPivo::mediana_de_tres);
+    else if (tipo_algoritmo == "QPE")
+        resultado = Quicksort::classico(vetor, n, EscolhaPivo::primeiro);
+    else if (tipo_algoritmo == "QI1")
+        resultado = Quicksort::com_insercao(vetor, n, 1);
+    else if (tipo_algoritmo == "QI5")
+        resultado = Quicksort::com_insercao(vetor, n, 5);
+    else if (tipo_algoritmo == "QI10")
+        resultado = Quicksort::com_insercao(vetor, n, 10);
+    else if (tipo_algoritmo == "QNR")
+        resultado = Quicksort::nao_recursivo(vetor, n);
+
+    print_vetor(vetor, 20);
+    
     std::cout << resultado.get_num_comparacoes() << " comparações | "
               << resultado.get_num_movimentacoes() << " movimentações | "
               << resultado.get_tempo_microseg() << " µs " << std::endl;
